5be6964bdf867da348287e5c1376f969b0d37dd2
Fivek88 <hafletjm01@gmail.com>
2023-09-17 11:47:36 -0400

    added basic text menu

--- Todo/main.go
+++ Todo/main.go
@@ -2,21 +2,67 @@ package main
 
 import (
 	"fmt"
+	"time"
+	"strings"
 )
 
+// Declaring the todo struct here I think that's everything.
 type todo struct {
 	name        string
 	completed   bool
-	timeEntered string
-	someText    string
+	timeEntered int[]
+	// small description about the Todo
+	someText string
 }
 
-func newTodo(name string) *todo {
-
+func newTodo(name, someText string) *todo {
+	t := todo{name: name}
+	t.completed = false
+	t.timeEntered = time.Date()
+	return &t
 }
+
+
+func mainMenu () string{
+	var input
+	print(`
+			========== Main Menu ==========
+			-------------------------------
+
+			A. Add a new Todo 
+			B. Load Todo File
+			C. Save Current Todos to File
+			D. Print Loaded Todos
+			Q. Quit
+
+			-------------------------------
+			========== Main Menu ==========
+	`)
+
+	fmt.Println("Please enter a choice: \n")
+	fmt.Scan(&input)
+	input := strings.ToUpper(input)
+	return input
+}	
 
 func main() {
+	var quit bool
 
-	fmt.Println("Hello World!")
-	fmt.Printf("The Todo Type %T", todo)
+	for !quit{
+		choice := mainMenu()
+		switch choice; {
+			case "A":  
+				fmt.Println("Add a todo selected.")
+			case "B":  
+				fmt.Println("Load Todo File selected.")
+			case "C":  
+				fmt.Println("Save Current Todos to File.")
+			case "D":  
+				fmt.Println("Print Loaded Todos.")
+			case "Q":  
+				quit := true
+			default:
+				fmt.Println("Input not recognized try again")
+		}
+	} 
 }
