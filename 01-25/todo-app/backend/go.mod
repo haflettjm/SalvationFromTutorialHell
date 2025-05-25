e5a7dfa9f11a1e65e5f9d48abd800178cabcc72d
Jacob Haflett <hafletjm01@gmail.com>
2025-05-25 00:09:46 -0400

    project 001 started

---
+++ 01-25/todo-app/backend/go.mod
@@ -0,0 +1,3 @@
+module github.io/haflettjm/SalvationFromTutorialHell/01-25/todo-app/back-end
+
+go 1.24.2
---
+++ 01-25/todo-app/backend/main.go
@@ -0,0 +1,11 @@
+package main
+
+import {
+	"fmt"
+	"net/http"
+	 "github.com/spf13/cobra"
+}
+
+func main(){
+	fmt.Println("Starting application...")
+}
