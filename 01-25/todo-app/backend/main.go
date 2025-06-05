0a612a3d71cff5bca465dc76b2c2bff40c8ae9a4
Jacob Haflett <hafletjm01@gmail.com>
2025-06-04 22:49:39 -0400

    added some changes and new project time

--- 01-25/todo-app/backend/main.go
+++ 01-25/todo-app/backend/main.go
@@ -3,23 +3,9 @@ package main
 import {
 	"fmt"
 	"net/http"
-	 "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
+	"errors"
 }
-
-type DataT struct{
-	data bytes
-	uuid string
-}
-
-type testMe interface{
-	loadData()
-}
-
-
-	uuid = "al;kjasdlkfjasdff"
-}
-obj1.TestMe()
-obj2.TestMe.Load
 
 func main(){
 	fmt.Println("Starting application...")
